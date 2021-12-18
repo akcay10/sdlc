912d9d3466ef1e45e6e091a5c650df2af65c3655
Unal <unalakcay1034@gmail.com>
2021-12-17 17:18:19 -0700

    First Version

---
+++ Day03.java
@@ -0,0 +1,77 @@
+package sdlc;
+
+public class Day03 {
+
+	public static void main(String[] args) {
+	/*
+	 
+	 SDLC
+		Agile and Scrum
+		
+		SDLC => Software Development Life Cycle
+		
+		Waterfall : it is an easier model to manage and implement, but hard to make changes or updates based on Customer feedbacks.
+		It is best applied to short term projects.
+		Agile : It is user friendly, when there new customer or business feedbacks, they will be always welcome
+		It is best applied to long term projects
+		
+		SCRUM TEAM => Dynamic Team
+		Self organized team
+		Product Owner : Representative of the Business, They may also be Investor in project
+		Scrum Master : Facilitator, they would help SM team to get on a good progress with all necessary meetings
+		Developer  : they write high quality codes.
+		QA  : they are assuring the product quality.
+		BA : They are managing documentations and they are responsible to update BRD and FRD
+		sprints => 2 - 4
+		Product Backlog : This is the list of items / user stories that The Dev team will work on during the whole project.
+		Sprint Backlog : this is the list of items to work during one specific sprint.
+		Jira is a project management tool
+		Meetings /
+		
+		google it
+		1) Grooming meeting / Refinement meeting:  The PO brings all user stories and the team analyses the user stories. They clarify their questions asking PO. We also do a general estimation about user stories.
+		2) Sprint planning : The Dev team point the user stories and they share the responsibilities.
+		The actual sprint starts with this meeting.
+		3) Daily scrum / Daily Stand-up
+		this is a daily meeting that lasts 15 minutes.
+		we need to talk about
+		what we did yesterday and what we are going to today?
+		we also talk about impediment..
+		it is just a daily uodate about Dev team members.
+		4) Sprint Review meeting / Demo:
+		The Dev team does a Demo to Business people and One person from the QA team presents the Demo.
+		5) Retrospective meeting
+		we evaluate the whole sprint.
+		what went well?
+		what could be improved?
+		what do we commit to doing next week?
+		any impedimnets we faced so far?
+		Tester positions
+		QA Tester positions
+		QA Analyst positions
+		Automation Tester
+		Manual Tester positions
+		SDET => Software Development Engineer in Test
+		Java Developer
+		Software Developer
+		Full Stack Developer
+		FrontEnd Developer
+		Backend Developer
+		
+		
+		Pointing
+	1 point = 8 hours of work
+	sprint duration => 2 weeks
+	5 Developers
+	team capacity => 50 points
+	velocity of the team : the curent team availibility / 30
+	
+	
+	
+
+	 
+	 */
+
+	}
+
+}
